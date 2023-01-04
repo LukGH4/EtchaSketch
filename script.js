6b6f087183cbe951fa4793426eaa21100904fa57
Luke <19198075+LukGH4@users.noreply.github.com>
2023-01-04 13:57:52 -0700

    Fix scaling for  script.js

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ function generateSquares () {
         let rowNum = document.querySelector(`.row-${i}`);
         for (let j = 0; j < numSquares; j++){
             rowNum.innerHTML += `<div class='col-${j} one-div'
-            style='width:clamp(5px, 9vmin, ${sqrSize}px); height:clamp(5px, 9vmin, ${sqrSize}px);'></div>`;
+            style='width:clamp(5px, 90vmin, ${sqrSize}px); height:clamp(5px, 90vmin, ${sqrSize}px);'></div>`;
         }
     }
 
     for (let i = 0; i < numSquares; i++) {
         for (let j = 0; j < numSquares; j++){
             const currDiv = document.querySelector(`.row-${i} .col-${j}`);
-            currDiv.onmousemove = () => {
+            currDiv.onmouseout = () => {
                 let passPercent = 90;
                 currDiv.style.filter += `brightness(${passPercent}%)`
             }
