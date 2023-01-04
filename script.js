159e9b9b315c8c09f5b2de7290cd1090938a0261
Luke <19198075+LukGH4@users.noreply.github.com>
2023-01-04 11:04:40 -0700

    Edit script.js and styles.css

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const numButton = document.querySelector('.div-num');
 numButton.addEventListener('click', generateSquares);
 
 function generateSquares () {
-    numSquares = prompt('What size grid of squares?');
+    let numSquares = prompt('What size grid of squares?');
     while (numSquares > 100 && numSquares < 0) {
         numSquares = prompt('Choose a number less than 100');
     }
     gridContainer.innerHTML = ''
     sqrSize = 640/numSquares;
-    height = 640/numSquares;
+    
+    console.log(sqrSize)
     for (let i = 0; i < numSquares; i++){
         gridContainer.innerHTML += `<div class='row-${i}'></div>`;
         let rowNum = document.querySelector(`.row-${i}`);
         for (let j = 0; j < numSquares; j++){
             rowNum.innerHTML += `<div class='col-${j} one-div'
-            style='width:${sqrSize}px; height:${sqrSize}px;'></div>`;
+            style='width:clamp(5px, 9vmin, ${sqrSize}px); height:clamp(5px, 9vmin, ${sqrSize}px);'></div>`;
         }
     }
 
@@ -30,6 +31,5 @@ function generateSquares () {
         }
     }
 }
-
 
 
